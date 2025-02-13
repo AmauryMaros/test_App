2e161aa5e08622d1795a2f1088ab48bbb8c83419
Amaury Maros <maros.amaury@gmail.com>
2025-02-13 13:21:10 -0500

    Update page0.py

--- pages/page0.py
+++ pages/page0.py
@@ -2,10 +2,12 @@ import dropbox
 import pandas as pd
 
 # Replace with your actual access token
-access_token = 'your_access_token'
+access_token = st.secrets["dropbox"]["access_token"]
 
 # Initialize Dropbox client
 dbx = dropbox.Dropbox(access_token)
+
+st.write(acces_token)
 
 # Path to the folder (adjust to your folder path)
 folder_path = '/path/to/folder'
